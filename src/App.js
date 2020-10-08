bac6f733c69f617a262dc5a39afed970808dbc1d
Moonlight <moonlightlightmoon@gmail.com>
2020-10-08 15:42:02 +0500

    complete header-carousel container

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from 'react';
+import Header from './container/Header/Header';
+import './App.css';
+import Carousel from './container/Carousel/Carousel';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
